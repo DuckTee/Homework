7aa5e7105daed8d3260da17e2b87c92d47b3449c
Ksenia_Sheveleva <brantova00@gmail.com>
2025-03-04 12:01:39 +0300

    add masks.py

---
+++ src/masks.py
@@ -0,0 +1,18 @@
+card_number = input()
+account_number = input()
+
+
+def get_mask_card_number(card_number: str) -> str:
+    """Функция маскировки номера банковской карты"""
+
+    return f'{card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]}'
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция маскировки номера банковского счета"""
+
+    return f'{"**" + account_number[-4:]}'
+
+
+print(get_mask_card_number(card_number))
+print(get_mask_account(account_number))
