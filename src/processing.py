c3e452ac38d22a727b78aeedfd399096caec9c4d
Ksenia_Sheveleva <brantova00@gmail.com>
2025-03-12 18:35:48 +0300

    A function sort_by_date has been created

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def filter_by_state(data_dictionary, state='EXECUTED'):
     '''Фильтрует список словарей по ключу 'state' и возвращает новый список'''
 
@@ -16,9 +18,27 @@ if __name__ == "__main__":
     ]
 
     result_default = filter_by_state(input_dictionary)
-    print("Выход функции со статусом по умолчанию 'EXECUTED':")
+    print("Выход функции filter_by_state со статусом по умолчанию 'EXECUTED':")
     print(result_default)
 
     result_canceled = filter_by_state(input_dictionary, 'CANCELED')
-    print("\nВыход функции, если вторым аргументом передано 'CANCELED':")
+    print("\nВыход функции filter_by_state, если статус 'CANCELED':")
     print(result_canceled)
+
+
+def sort_by_date(data, reverse=True):
+    '''Функция возвращает новый список, отсортированный по дате'''
+
+    return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)
+
+data = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}
+]
+
+sorted_data_desc = sort_by_date(data)
+print("\nВыход функции sort_by_date:")
+for sorted_data in sorted_data_desc:
+    print(sorted_data)
