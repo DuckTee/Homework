c7b448f55ca56e86db2172e317275871d1ead214
Ksenia_Sheveleva <brantova00@gmail.com>
2025-03-13 16:35:33 +0300

    adding annotations to functions

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from typing import Any, Dict, List
 
 
-def filter_by_state(data_dictionary, state='EXECUTED'):
+def filter_by_state(data_dictionary: List[Dict[str, object]], state: str = 'EXECUTED') -> List[Dict[str, object]]:
     '''Фильтрует список словарей по ключу 'state' и возвращает новый список'''
 
     filtered_dictionary = []
@@ -28,7 +29,7 @@ if __name__ == "__main__":
     print(result_canceled)
 
 
-def sort_by_date(data, reverse=True):
+def sort_by_date(data: List[Dict[str, Any]], reverse=True) -> List[Dict[str, Any]]:
     '''Функция возвращает новый список, отсортированный по дате'''
 
     return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)
